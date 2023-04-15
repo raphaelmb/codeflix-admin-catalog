e52b3edb518800a8eba73039f45b46d2fe920e86
Raphael Berger <raphaelmb@gmail.com>
2023-04-15 16:56:56 -0300

    search result

--- src/@seedwork/domain/repository/repository-contracts.spec.ts
+++ src/@seedwork/domain/repository/repository-contracts.spec.ts
@@ -1,4 +1,4 @@
-import { SearchParams } from "./repository-contracts";
+import { SearchParams, SearchResult } from "./repository-contracts";
 
 describe("SearchParams Unit Tests", () => {
   test("page prop", () => {
@@ -128,5 +128,79 @@ describe("SearchParams Unit Tests", () => {
         item.expected
       );
     });
+  });
+});
+
+describe("SearchResult Unit Tests", () => {
+  test("constructor props", () => {
+    let result = new SearchResult({
+      items: ["entity1", "entity2"] as any,
+      total: 4,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    });
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ["entity1", "entity2"] as any,
+      total: 4,
+      currentPage: 1,
+      perPage: 2,
+      lastPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    });
+
+    result = new SearchResult({
+      items: ["entity1", "entity2"] as any,
+      total: 4,
+      currentPage: 1,
+      perPage: 2,
+      sort: "name",
+      sortDir: "asc",
+      filter: "test",
+    });
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ["entity1", "entity2"] as any,
+      total: 4,
+      currentPage: 1,
+      perPage: 2,
+      lastPage: 2,
+      sort: "name",
+      sortDir: "asc",
+      filter: "test",
+    });
+  });
+
+  it("should set lastPage 1 to 1 when perPage field is greater than total field", () => {
+    const result = new SearchResult({
+      items: [] as any,
+      total: 4,
+      currentPage: 1,
+      perPage: 15,
+      sort: "name",
+      sortDir: "asc",
+      filter: "test",
+    });
+
+    expect(result.lastPage).toBe(1);
+  });
+
+  test("lastPage prop when total is not a multiple of perPage", () => {
+    const result = new SearchResult({
+      items: [] as any,
+      total: 101,
+      currentPage: 1,
+      perPage: 20,
+      sort: "name",
+      sortDir: "asc",
+      filter: "test",
+    });
+
+    expect(result.lastPage).toBe(6);
   });
 });
--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -95,10 +95,56 @@ export class SearchParams {
   }
 }
 
+type SearchResultProps<E extends Entity, Filter> = {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+};
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    };
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchOutput,
-  SearchInput = SearchParams
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<E, Filter>
 > extends RepositoryInterface<E> {
   search(props: SearchInput): Promise<SearchOutput>;
 }
